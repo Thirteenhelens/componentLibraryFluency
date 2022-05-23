0afc255e374522b8be332f64fa56ba07b28abc35
Jack Barrett <jbarrett1347@gmail.com>
2022-05-23 14:53:27 -0500

    added better button routing to the nav, added component styling to component buttons, and cleaned some code up

--- componentcenter/src/HomePage/HomePage.jsx
+++ componentcenter/src/HomePage/HomePage.jsx
@@ -1,27 +1,56 @@
+// importing css styling
 import "./HomePage.css";
-import { Link } from "react-router-dom";
+
+// import needed button components & React
+import React from "react";
+import { Button as AntButton } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Button as RebassButton } from "rebass";
+import { Button as MuiButton } from "@mui/material";
+import { Button as BootButton } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function HomePage() {
+  let navigate = useNavigate();
+
+  const toMui = () => {
+    navigate("/materialui");
+  };
+  const toBoot = () => {
+    navigate("/reactbootstrap");
+  };
+  const toRebass = () => {
+    navigate("/rebass");
+  };
+  const toAnt = () => {
+    navigate("/antdesign");
+  };
+
   return (
-    <>
+    <div className="homePage">
       <div className="head">
         <h1>ThirteenHelens LLC</h1>
       </div>
 
-      <div className="nav">
-        <div className="mui">
-          <Link to="/materialui">MUI</Link>
-        </div>
-        <div className="reactB">
-          <Link to="/reactbootstrap">React Bootstrap</Link>
-        </div>
-        <div className="rebass">
-          <Link to="/rebass">Rebass</Link>
-        </div>
-        <div className="antD">
-          <Link to="/antdesign">Ant Design</Link>
-        </div>
-      </div>
+      <nav>
+        <MuiButton variant="contained" onClick={toMui}>
+          MUI
+        </MuiButton>
+        <BootButton variant="primary" onClick={toBoot}>
+          React Bootstrap
+        </BootButton>
+        <RebassButton
+          bg="blue"
+          color="white"
+          variant="outline"
+          onClick={toRebass}
+        >
+          Rebass
+        </RebassButton>
+        <AntButton type="primary" onClick={toAnt}>
+          Ant Design
+        </AntButton>
+      </nav>
 
       <div className="aboutSection">
         <h2>What is ThirteenHelens?</h2>
@@ -31,6 +60,6 @@ export default function HomePage() {
 
         <div className="resume"></div>
       </div>
-    </>
+    </div>
   );
 }
